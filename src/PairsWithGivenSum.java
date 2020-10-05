fccc845977adcca87416bbb9c96cb302729ebbe7
Karan Mehta <mehtakaran9@gmail.com>
2020-10-05 06:30:01 -0700

    Adding gitignore and pairs with given sum

---
+++ src/PairsWithGivenSum.java
@@ -0,0 +1,22 @@
+public class PairsWithGivenSum {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 7, -1};
+        int sum = 6;
+        System.out.println(new PairsWithGivenSum().findNumberOfPairsWithGivenSum(arr, sum));
+
+    }
+    
+    private int findNumberOfPairsWithGivenSum(int[] arr, int sum) {
+        int totalPairs = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int a = sum - arr[i];
+            for (int j = i+1; j < arr.length; j++) {
+                if(a == arr[j]) {
+                    totalPairs++;
+                }
+            }
+        }
+        return totalPairs;
+	};
+    
+}
