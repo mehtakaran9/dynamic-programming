e456035a37f4fb07f15df21841f53cecfcdbd793
Karan Mehta <mehtakaran9@gmail.com>
2020-10-05 06:11:35 -0700

    Maximum number of games that can be played

---
+++ src/MaximumNumberOfGames.java
@@ -0,0 +1,16 @@
+public class MaximumNumberOfGames {
+    public static void main(String[] args) {
+        System.out.println(new MaximumNumberOfGames().calcuateMaxGames(6));
+    }
+
+    private int calcuateMaxGames(int n) {
+        float[] arr = new float[n+1];
+        arr[0] = 0;
+        arr[1] = 0;
+        arr[2] = 1;
+        for (float i = 3; i <= n; i++) {
+            arr[(int) i] = 1 + arr[Math.round(i/2)];
+        }
+        return (int) arr[n];
+    }
+}
