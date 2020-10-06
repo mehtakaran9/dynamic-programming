c43b3ad8b63f51c53a06f1699e055f25f5ab1939
Karan Mehta <karan.mehta@gdn-commerce.com>
2020-10-07 01:31:32 +0530

    Changes

--- src/main/java/dynamicprogramming/UglyNumbers.java
+++ src/main/java/dynamicprogramming/UglyNumbers.java
@@ -7,32 +7,26 @@ public class UglyNumbers {
 
   private int printNthUglyNumber(int n) {
     int[] arr = new int[n + 1];
-    arr[0] = 0;
+    arr[0] = 1;
     arr[1] = 1;
-    int i = 2;
-    int start = 2;
-    while (start <= n) {
-      int temp = i;
-      if (temp % 2 == 0) {
-        while (temp % 2 == 0) {
-          temp = temp / 2;
-        }
+    int i2 = 1, i3 = 1, i5 = 1;
+    int nm2 = i2 * 2;
+    int nm3 = i3 * 3;
+    int nm5 = i5 * 5;
+    for (int i = 2; i <= n; i++) {
+      arr[i] = Math.min(Math.min(nm2, nm3), nm5);
+      if (arr[i] == nm2) {
+        i2++;
+        nm2 = arr[i2] * 2;
       }
-      if (temp % 3 == 0) {
-        while (temp % 3 == 0) {
-          temp = temp / 3;
-        }
+      if (arr[i] == nm3) {
+        i3++;
+        nm3 = arr[i3] * 3;
       }
-      if (temp % 5 == 0) {
-        while (temp % 5 == 0) {
-          temp = temp / 5;
-        }
+      if (arr[i] == nm5) {
+        i5++;
+        nm5 = arr[i5] * 5;
       }
-      if (temp == 1) {
-        arr[start] = i;
-        start++;
-      }
-      i++;
     }
     return arr[n];
   }
